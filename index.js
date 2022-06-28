dcc617ef038567e59dbad4cd65ec9981ed74bfd0
Nkombe <55114119+nkombemaro@users.noreply.github.com>
2022-06-28 10:38:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// var number=5;
+// let name="Shawan";
+// number12;
+// name="Pluralsight"
+// //wriring to the document
+// document.writeln(name + number)
+// //alert("Hello world")
+
+
+
+let hasjob="true";
+if(hasjob){
+    //I have a job
+    document.writeln("Thanks for visiting, I have a job.");
+}
+else{
+    //I need a job
+    document.writeln("Please look around, I'm currently looking for a position");
+}
+
+let today= new Date():
+let dayOfWeek=today.getDay():
+if (dayOfWeek===0) and dayOfWeek===6{
+    document.writeln("Since it is a weekend, please be patient, I will return email")
+
+}
